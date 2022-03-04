8c73c25d490f241434f2829e0ba6430b56548c76
CSpiegelhalter <spiegelhaltercurt@gmail.com>
2022-03-04 10:28:20 -0600

    Added promise to db query

--- amplify/backend/function/netflixmovies/src/index.js
+++ amplify/backend/function/netflixmovies/src/index.js
@@ -1,33 +1,33 @@
 /* Amplify Params - DO NOT EDIT
-	ENV
-	REGION
-	database
-	username
-	password
-	host
-	port
+  ENV
+  REGION
+  database
+  username
+  password
+  host
+  port
 Amplify Params - DO NOT EDIT */
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 
- const Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 
 
 
- exports.handler = async (event) => {
-     // console.log(`EVENT: ${JSON.stringify(event)}`);
-     
- var sequelize = await new Sequelize('moivepicker-dev', 'admin', 'password', {
+exports.handler = async (event) => {
+  // console.log(`EVENT: ${JSON.stringify(event)}`);
+
+  var sequelize = await new Sequelize('moivepicker-dev', 'admin', 'password', {
     host: 'moivepicker-dev.clgftnbfrrf4.us-east-1.rds.amazonaws.com',
     port: 3306,
     logging: console.log,
     maxConcurrentQueries: 100,
     dialect: 'mysql',
     dialectOptions: {
-        ssl:'Amazon RDS',
-        // ssl: { rejectUnauthorized: false }
+      ssl: 'Amazon RDS',
+      // ssl: { rejectUnauthorized: false }
     },
     pool: {
       max: 15,
@@ -37,54 +37,53 @@ Amplify Params - DO NOT EDIT */
       acquire: 30000
     },
     language: 'en'
-})
-let db = {};
+  })
+  let db = {};
 
 
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+  db.Sequelize = Sequelize;
+  db.sequelize = sequelize;
 
-db.user = require("./rest/user")(sequelize, Sequelize);
-db.netflixMovies = require("./rest/netflixMovies")(sequelize, Sequelize);
+  db.user = require("./rest/user")(sequelize, Sequelize);
+  db.netflixMovies = require("./rest/netflixMovies")(sequelize, Sequelize);
 
-await db.sequelize.authenticate().then(() => {
-  console.log("Connected to the database!");
-})
-.catch(err => {
-  console.log('random log');
-  console.log("Cannot connect to the database!", err);
-  process.exit();
-});
+  await db.sequelize.authenticate().then(() => {
+    console.log("Connected to the database!");
+  })
+    .catch(err => {
+      console.log('random log');
+      console.log("Cannot connect to the database!", err);
+      process.exit();
+    });
 
 
-// sync
-db.sequelize.sync()
-     
- 
- // module.exports = db;
- 
- console.log("here perhaps?")
-     db.netflixMovies.findAll({
-        where: {
-           id: 2
-          }
-     })
-         .then((data) => {
-           console.log("HELOO??????")
-           console.log(JSON.stringify(data))
-             return JSON.stringify(data)
-         }).catch(err => {
-             console.log("WHOOPS", err);
-         })
-    //      console.log('fuckin here')
-     // return {
-     //     statusCode: 200,
-     // //  Uncomment below to enable CORS requests
-     // //  headers: {
-     // //      "Access-Control-Allow-Origin": "*",
-     // //      "Access-Control-Allow-Headers": "*"
-     // //  }, 
-     //     body: JSON.stringify('Hello from Lambda!'),
-     // };
- };
- 
+  // sync
+  db.sequelize.sync()
+
+
+  // module.exports = db;
+
+  console.log("here perhaps?")
+  await db.netflixMovies.findAll({
+    where: {
+      id: 2
+    }
+  })
+    .then((data) => {
+      console.log("HELOO??????")
+      console.log(JSON.stringify(data))
+      return JSON.stringify(data)
+    }).catch(err => {
+      console.log("WHOOPS", err);
+    })
+  //      console.log('fuckin here')
+  // return {
+  //     statusCode: 200,
+  // //  Uncomment below to enable CORS requests
+  // //  headers: {
+  // //      "Access-Control-Allow-Origin": "*",
+  // //      "Access-Control-Allow-Headers": "*"
+  // //  }, 
+  //     body: JSON.stringify('Hello from Lambda!'),
+  // };
+};
