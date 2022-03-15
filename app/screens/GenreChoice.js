7c605b1d4a06690fef93551956d2435d6b05ee91
CSpiegelhalter <spiegelhaltercurt@gmail.com>
2022-03-15 12:45:47 -0500

    Added state to track genre selections. You can select/deslect.

--- app/screens/GenreChoice.js
+++ app/screens/GenreChoice.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, View, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StyleSheet, View, FlatList, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Genres from '../components/Genres';
 
@@ -26,6 +26,22 @@ const genres = [
 
 function GenreChoice() {
     const navigation = useNavigation();
+    const [genreSelect, setGenreSelect] = useState([])
+
+    useEffect(() => {
+
+    }, [genreSelect]);
+
+    const editServices = (genre) => {
+        if (!(genreSelect.includes(genre))) {
+            setGenreSelect([...genreSelect, genre])
+        }
+        else {
+            let holder = genreSelect.filter((a) => a !== genre)
+            setGenreSelect(holder)
+        }
+        console.log(genreSelect);
+    }
 
     return (
         <View style={{ flex: 1, width: '100%', justifyContent: 'center', alignItems: 'center', height: '100%' }} >
@@ -37,9 +53,13 @@ function GenreChoice() {
                     renderItem={({ item }) =>
                         <Genres
                             genreName={item}
-                            onPress={() => console.log('service')}
+                            onPress={() => editServices(item)}
                         />
                     }
+                />
+                <Button
+                title="Next: Choose movies/shows" 
+                onPress={() => console.log(genreSelect)}
                 />
             </SafeAreaView>
         </ View>
@@ -51,7 +71,8 @@ const styles = StyleSheet.create({
     container: {
         width: '100%',
         height: '100%',
-        flexDirection: 'row',
+        // flexDirection: 'row',
+        // justifyContent: 'center'
     },
     scroll: {
         width: '100%',
