46700680e32e7c698e3b5c708c4379c0dfd6836e
CSpiegelhalter <spiegelhaltercurt@gmail.com>
2022-03-20 08:09:18 -0500

    Put small carousel into it's own component. Rendered each selected genre into own flatlist

--- app/components/Movie.js
+++ app/components/Movie.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, View, TouchableWithoutFeedback, FlatList, Button } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Image, Dimensions, TouchableWithoutFeedback, FlatList, Button } from 'react-native';
 // import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import Carousel from 'react-native-snap-carousel/src/carousel/Carousel';
 
-function Movie({ url }) {
 
+const SLIDER_WIDTH = Dimensions.get('window').width;
+const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7);
+const ITEM_HEIGHT = Math.round(ITEM_WIDTH * 3 / 4);
 
+function Movie({ movieArray }) {
+    console.log('hey?');
+
+    var renderItem = (item, index) => {
+        console.log(item.item['thumbnail']);
+        // console.log(movies['Action']);
+        return (
+            <View style={styles.itemContainer}>
+                <Image style={{height: '100%', width: '100%'}} source={{ uri: item.item['thumbnail'] }} />
+
+            </View>
+        );
+    }
     
     return (
         <>
-            <TouchableWithoutFeedback>
-                <Image style={styles.container} source={url} />
-            </TouchableWithoutFeedback>
-
+            <Carousel
+                    //   ref={(c) => { this._carousel = c; }}
+                    data={movieArray}
+                    renderItem={renderItem}
+                    sliderWidth={1000}
+                    containerCustomStyle={styles.carouselContainer}
+                    itemWidth={ITEM_WIDTH}
+                // itemHeight={250}
+            />
+  
 
         </>
     );
@@ -19,11 +41,16 @@ function Movie({ url }) {
 
 const styles = StyleSheet.create({
 
-    container: {
-        width: '75%',
-        height: '75%',
-
-    }
+    carouselContainer: {
+        marginTop: 50
+    },
+    itemContainer: {
+        width: ITEM_WIDTH,
+        height: ITEM_HEIGHT,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'dodgerblue'
+    },
 })
 
 export default Movie;
--- app/screens/MovieBrowse.js
+++ app/screens/MovieBrowse.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { SafeAreaView, StyleSheet, View, Text, Image, Dimensions, FlatList, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Movie from '../components/Movie';
-import Carousel from 'react-native-snap-carousel/src/carousel/Carousel';
 import Genres from '../components/Genres';
 import AppText from '../components/AppText';
 import allMovies from '../api/allMovies';
@@ -17,36 +16,27 @@ function MovieBrowser({ route }) {
     const navigation = useNavigation();
     const movies = route.params.movies
     const movieKeys = Object.keys(movies);
+    console.log(movieKeys);
     // console.log(movies['Crime'][0]['thumbnail']);
 
 
 
-    var renderItem = (item, index) => {
-        console.log(item.item['thumbnail']);
-        // console.log(movies['Action']);
-        return (
-            <View style={styles.itemContainer}>
-                <Image style={{height: '100%', width: '100%'}} source={{ uri: item.item['thumbnail'] }} />
-
-            </View>
-        );
-    }
+    
 
     return (
         <View style={{ flex: 1, width: '100%', justifyContent: 'center', alignItems: 'center', height: '100%' }} >
             <SafeAreaView style={styles.container}>
-                {/* {movieKeys.map((item, index) => { */}
-                <Carousel
-                    //   ref={(c) => { this._carousel = c; }}
-                    data={movies['Action']}
-                    renderItem={renderItem}
-                    sliderWidth={1000}
-                    containerCustomStyle={styles.carouselContainer}
-                    itemWidth={ITEM_WIDTH}
-                // itemHeight={250}
+            <FlatList
+                    nestedScrollEnabled={true}
+                    data={movieKeys}
+                    keyExtractor={movie => movie}
+                    renderItem={({ item }) =>
+                        <Movie
+                            movieArray={movies[item]}
+                            // onPress={() => editServices(item)}
+                        />
+                    }
                 />
-                {/* }) */}
-
 
             </SafeAreaView>
         </ View>
