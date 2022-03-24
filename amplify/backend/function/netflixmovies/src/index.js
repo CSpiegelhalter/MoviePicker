1266fdbdc92b8b3e413ae02019a75d0339193093
CSpiegelhalter <spiegelhaltercurt@gmail.com>
2022-03-24 09:21:07 -0500

    Changed db structure, changing query accordingly

--- amplify/backend/function/netflixmovies/src/index.js
+++ amplify/backend/function/netflixmovies/src/index.js
@@ -23,21 +23,53 @@ exports.handler = async (event, context, callback) => {
   service = service.split(',')
   genreList = genreList.split(',')
 
-  for (let i = 0; i <= service.length - 1; i++) {
-    for (let j = 0; j <= genreList.length - 1; j++) {
-      let hold = service[i]
-      const returnData = await db[hold].findAll({
-        order: db.sequelize.random(),
-        limit: 10,
-        benchmark: true,
-        logging: console.log,
-        where: {
-          genres: db.sequelize.where(db.sequelize.fn('LOWER', db.sequelize.col('genres')), 'LIKE', '%' + genreList[j] + '%')
-        }
-      })
-      movieDate[genreList[j]] = returnData
-    }
+
+  let ServiceConditionalArray = []
+  let GenreConditionalArray = []
+  for (let i = 0; i < service.length; i++) {
+    ServiceConditionalArray.push({Service: {
+      [Op.like]: '%' + service[i] + '%'
+    }})
   }
+
+  for (let i = 0; i < genreList.length; i++) {
+    GenreConditionalArray.push({Genres: {
+      [Op.like]: '%' + genreList[i] + '%'
+    }})
+  }
+
+  let condition = {
+    [Op.and]: {
+      [Op.or]: ServiceConditionalArray,
+      [Op.or]: GenreConditionalArray
+    }
+    
+  }
+
+  const returnData = await db.netflixMovies.findAll({
+          order: db.sequelize.random(),
+          limit: 10,
+          benchmark: true,
+          logging: console.log,
+          where: condition
+        })
+
+
+  // for (let i = 0; i <= service.length - 1; i++) {
+  //   for (let j = 0; j <= genreList.length - 1; j++) {
+  //     let hold = service[i]
+  //     const returnData = await db[hold].findAll({
+  //       order: db.sequelize.random(),
+  //       limit: 10,
+  //       benchmark: true,
+  //       logging: console.log,
+  //       where: {
+  //         genres: db.sequelize.where(db.sequelize.fn('LOWER', db.sequelize.col('genres')), 'LIKE', '%' + genreList[j] + '%')
+  //       }
+  //     })
+  //     movieDate[genreList[j]] = returnData
+  //   }
+  // }
 
 
   return {
